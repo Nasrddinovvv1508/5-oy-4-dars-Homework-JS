6617e73284d0b729a3a40de66fd2812031602bbe
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-05-02 23:02:36 +0500

    project

---
+++ static/JS/index.js
@@ -0,0 +1,177 @@
+// 1-MASALA
+// function sum(x, y) {
+//     if (!isNaN(x) && !isNaN(y)) {
+//         return x + y;
+//     } else {
+//         return `Error`
+//     }
+// }
+
+// console.log(sum(`2`, 3));
+
+
+// 2-MASALA
+// function toSekunds(minutes) {
+//     if (!isNaN(minutes)) {
+//         return Math.trunc(minutes) * 60
+//     } else {
+//         return `Error`
+//     }
+// }
+
+// console.log(toSekunds(`5`));
+
+
+// 3-MASALA
+// function nextInteger(int) {
+//     if (!isNaN(int)) {
+//         return int + 1
+//     } else {
+//         return `Error`
+//     }
+// }
+
+// console.log(nextInteger(`3`));
+
+
+// 4-MASALA
+// function uchburchakYuzi(asos, balandlik) {
+//     if (!isNaN(asos) && !isNaN(balandlik)) {
+//         return Math.round((asos * balandlik) / 2);
+//     } else {
+//         return `Error`  
+//     }
+// }
+
+// console.log(uchburchakYuzi(10, 10));
+
+
+// 5-MASALA
+// function ageToDays(age) {
+//     return age * 365;
+// }
+
+// console.log(ageToDays(65));
+
+
+// 6-MASALA
+// function returnCube(arg) {
+//     return Math.pow(arg, 3);
+// }
+
+// console.log(returnCube(3));
+
+
+// 7-MASALA
+// function firstElement(arr) {
+//     return arr[0];
+// }
+
+// console.log(firstElement([1, 2, 3, 4, 5]));
+
+
+// 8-MASALA
+// function power(x, y) {
+//     return x * y;
+// }
+
+// console.log(power(2, 3));
+
+
+// 9-MASALA
+// function hourToSekunds(hour) {
+//     return hour * 3600;
+// }
+
+// console.log(hourToSekunds(2));
+
+
+// 10-MASALA
+// function uchinchiTomon(tomon1, tomon2) {
+//     return tomon1 + tomon2 - 1
+// }
+
+// console.log(uchinchiTomon(8, 10));
+
+
+// 11-MASALA
+// function qoldiq(son1, son2) {
+//     return son1 % son2
+// }
+
+// console.log(qoldiq(1, 3));
+
+
+// 12-MASALA
+// function turtburchakYuzi(boyi, eni) {
+//     return Math.round(boyi * eni)
+// }
+
+// console.log(turtburchakYuzi(1, 2));
+
+
+// 13-MASALA
+// function stringQoshish(a) {
+//     return `Something ${a}`;
+// }
+
+// console.log(stringQoshish(`Hello`));
+
+
+// 14-MASALA
+// function returnLevel2(n) {
+//     return Math.pow(n, 2);
+// }
+
+// console.log(returnLevel2(3));
+
+
+// 15-MASALA
+// function noldan(raqam) {
+//     if (raqam <= 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(noldan(3));
+
+
+// 16-MASALA
+// function ichkiBurchaklar(n) {
+//     return Math.round((n - 2) * 180);
+// }
+
+// console.log(ichkiBurchaklar(3));
+
+
+// 17-MASALA
+// function ochkolar(x, y) {
+//     return (x * 2) + (y * 3);
+// }
+
+// console.log(ochkolar(1, 1));
+
+
+// 18-MASALA
+// function nameString(arg) {
+//     return `${arg}Edabit`
+// }
+
+// console.log(nameString("javaScript"));
+
+
+// 19-MASALA
+// function ikkitaSon(x, y) {
+//     if (x + y < 100) {
+//         return true;
+//     } else {
+//         return false
+//     }
+// }
+
+// console.log(ikkitaSon(1, 101));
+
+
+// 20-MASALA 
