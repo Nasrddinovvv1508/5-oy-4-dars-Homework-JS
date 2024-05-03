4750efd87615381556e3db96209240bd86cc0b70
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-05-03 14:35:22 +0500

    All matters done

--- static/JS/index.js
+++ static/JS/index.js
@@ -243,3 +243,275 @@
 // }
 
 // console.log(isTub(9));
+
+
+// 27-MASALA
+// function tengMi(x, y) {
+//     if (x === y) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(tengMi(1, `1`));
+
+
+// 28-MASALA
+// function booleanToString(bool) {
+//     return `${bool}`
+// }
+
+// console.log(typeof booleanToString(true));
+
+
+// 29-MASALA
+// let arrowFn = (arg) => {
+//     return arg;
+// }
+
+// console.log(arrowFn(true));
+
+
+// 30-MASALA
+// function frameSoni(minut, frame) {
+//    return Math.round(frame * (minut * 60))
+// }
+
+// console.log(frameSoni(10, 25));
+
+
+// 31-MASALA
+// function calc(str) {
+//     return eval(str);
+// }
+
+// console.log(calc(`23+1`));
+
+
+// 32-MASALA
+// function teng10(son1, son2) {
+//     if (son1 == 10 || son2 == 10 || son1 + son2 == 10) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(teng10(9, 1));
+
+
+// 33-MASALA
+// function litrMasofa(km) {
+//     return km * 10;
+// }
+
+// console.log(litrMasofa(12));
+
+
+// 34-MASALA
+// 1-usul
+// function returnMax(arg1, arg2) {
+//     let max = arg1;
+//     if (max < arg2) {
+//         max = arg2
+//     }
+
+//     return max;
+// }
+
+// console.log(returnMax(1, -3));
+
+// 2-usul
+// function returnMax(arg1, arg2) {
+//     let max;
+//     if (arg1 > arg2) {
+//         max = arg1
+//     } else {
+//         max = arg2
+//     }
+
+//     return max;
+// }
+
+// console.log(returnMax(1, -3));
+
+
+
+// 35-MASALA
+// function createArr(arg1, arg2) {
+//     return `${arg1}${arg2}`.split('');
+// }
+
+// console.log(createArr(1, 2));
+
+
+// 37-MASALA
+// function boshStr(str) {
+//     if (str == '') {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(boshStr(' '));
+
+
+// 38-MASALA
+// function bolinsin5(son) {
+//     if (son % 5 == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(bolinsin5(45));
+
+
+
+// 39-MASALA
+// function bolinsin100(son) {
+//     if (son % 100 == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(bolinsin100(101));
+
+
+
+// 40-MASALA
+// let str = `hello`
+// let i = 0
+
+// function uzunlik(arg) {
+//     i++;
+//     // console.log(i);
+//     if (i == arg.length) {
+//         console.log(i);
+//         return;
+//     }
+//     uzunlik(arg);
+// }
+
+// uzunlik(str)
+
+
+// 41-MASALA
+// function bolinsin(x, y) {
+//     if (x > y) {
+//         return `false 407`;
+//     }
+
+//     if (y % x == 0) {
+//         return true
+//     } else {
+//         return `false 413`
+//     }
+// }
+
+// console.log(bolinsin(10, 101));
+
+
+// 42-MASALA
+// function raqam(str) {
+//     return +str
+// }
+
+// console.log(raqam('3'));
+
+
+// 43-MASALA
+// function tortYuzi(a, b) {
+//     if (a <= 0 || b <= 0) {
+//         return -1
+//     }
+
+//     return a * b;
+// }
+
+// console.log(tortYuzi(2, -4));
+
+
+// 44-MASALA
+// function ismFam(ism, familiya) {
+//     return `${ism}, ${familiya}`
+// }
+
+// console.log(ismFam(`Najmiddin`, `Nasriddinov`));
+
+
+// 45-MASALA
+// function fn(arg) {
+//     if (arg == true) {
+//         return "sad days"
+//     } else {
+//         return `it's a good day`
+//     }
+// }
+
+// console.log(fn(true));
+
+
+// 46-MASALA
+// function tushirMassiv(arr, tushirishSoni) {
+//     let newArr = []
+//     for (let i = tushirishSoni; i < arr.length; i++) {
+//         newArr.push(arr[i]);
+//     }
+
+//     return newArr
+// }
+
+// console.log(tushirMassiv([1, 2, 3], 1));
+
+
+// 47-MASALA
+// function ovozlar(ovozObj) {
+//     return ovozObj.upvotes - ovozObj.downvotes;
+// }
+
+// console.log(ovozlar({ upvotes: 2, downvotes: 33}));
+
+
+// 48-MASALA
+// function negativ(son) {
+//     if (son > 0) {
+//         return -son;
+//     } else {
+//         return son;
+//     }
+// }
+
+// console.log(negativ(-4));
+
+
+// 49-MASALA
+// let arr = [1, 2, 3, 4, 5];
+
+// function almash(arr) {
+//     let newArr = [];
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         newArr.push(arr[i]);
+//     }
+
+//     return newArr;
+// }
+
+// console.log(almash(arr));
+
+
+// 50-MASALA 
+// function kinogaKirish(yosh, otaOna) {
+//     if (yosh > 15 || otaOna == true) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(kinogaKirish(14, true));
